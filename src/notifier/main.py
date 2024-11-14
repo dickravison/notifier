56efafc49c3cb723a3a62e0c412cfd8f2e93dab7
dickravison <78479145+dickravison@users.noreply.github.com>
2024-11-14 08:55:34 +0000

    Setting headers into global var

--- src/notifier/main.py
+++ src/notifier/main.py
@@ -5,10 +5,12 @@ import requests
 AWS_SESSION_TOKEN = os.environ.get('AWS_SESSION_TOKEN')
 SSM_URL = 'http://localhost:2773/systemsmanager/parameters/get?withDecryption=true&name='
 API_URL = 'https://api.pushover.net/1/messages.json'
+HEADERS = {
+    "User-Agent": "Python"
+}
 USER_PARAM = '/notifier/user'
 TOKEN_PARAM = '/notifier/token'
 
-#Loop over each record in the event and push the message to Pushover
 def notify(event, context):
     for e in event['Records']:
         headers = {'X-Aws-Parameters-Secrets-Token':AWS_SESSION_TOKEN}
@@ -20,5 +22,5 @@ def notify(event, context):
         message = e['Sns']['Message']
         subject = e['Sns']['Subject']
         payload = {"title": subject, "message": message, "user": user['Parameter']['Value'], "token": token['Parameter']['Value'] , "html": "1"}
-        r = requests.post(API_URL, data=payload, headers={'User-Agent': 'Python'})
+        r = requests.post(API_URL, data=payload, headers=HEADERS)
         print(r)
