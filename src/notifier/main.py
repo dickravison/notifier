e79d43b2cc231dc1eedf6f6c25df5144c67a997b
Rick <rick@dvsn.io>
2024-11-14 16:27:38 +0000

    Refactoring

--- src/notifier/main.py
+++ src/notifier/main.py
@@ -3,22 +3,26 @@ import os
 import requests
 
 AWS_SESSION_TOKEN = os.environ.get('AWS_SESSION_TOKEN')
+NOTIFICATIONS_ENABLED = os.environ.get('NOTIFICATIONS_ENABLED')
 SSM_URL = 'http://localhost:2773/systemsmanager/parameters/get?withDecryption=true&name='
 API_URL = 'https://api.pushover.net/1/messages.json'
+HEADERS = {
+    "User-Agent": "Python"
+}
 USER_PARAM = '/notifier/user'
 TOKEN_PARAM = '/notifier/token'
 
-#Loop over each record in the event and push the message to Pushover
 def notify(event, context):
+    #Retrieve the API credentials from AWS Parameter Store
+    headers = {'X-Aws-Parameters-Secrets-Token':AWS_SESSION_TOKEN}
+    response = requests.get(SSM_URL + USER_PARAM, headers = headers)
+    user = json.loads(response.text)
+    response = requests.get(SSM_URL + TOKEN_PARAM, headers = headers)
+    token = json.loads(response.text)
+    #Loop over each event and publish a new message to the SNS topic
     for e in event['Records']:
-        headers = {'X-Aws-Parameters-Secrets-Token':AWS_SESSION_TOKEN}
-        response = requests.get(SSM_URL + USER_PARAM, headers = headers)
-        user = json.loads(response.text)
-        response = requests.get(SSM_URL + TOKEN_PARAM, headers = headers)
-        token = json.loads(response.text)
-
         message = e['Sns']['Message']
         subject = e['Sns']['Subject']
         payload = {"title": subject, "message": message, "user": user['Parameter']['Value'], "token": token['Parameter']['Value'] , "html": "1"}
-        r = requests.post(API_URL, data=payload, headers={'User-Agent': 'Python'})
+        r = requests.post(API_URL, data=payload, headers=HEADERS)
         print(r)
